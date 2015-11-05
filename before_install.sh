c3c029ec729be658c10b646bf8ddc02cdf349080
Pihla Toivanen <toivanenpihla@gmail.com>
2015-11-05 20:01:44 +0200

    mutuo

--- before_install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/hy-ohtu/PaivanUutiset.git
-mvn clean install -U -f PaivanUutiset/pom.xml
-git clone https://github.com/hy-ohtu/HackerNewsUutiset.git
-mvn clean install -U -f HackerNewsUutiset/pom.xml
